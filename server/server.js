ecc1460e02d82796866d2979fcd0dbb13b6ee98c
abiona01 <quadratwemimo@gmail.com>
2023-10-01 13:38:20 +0100

    updates

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const express = require("express");
-const colors = require("colors");
-const connectDB = require("./config/db");
-const itemRoutes = require("./route/itemRoutes");
+require('dotenv').config();
+const express = require('express');
+const colors = require('colors');
+const connectDB = require('./config/db');
+const itemRoutes = require('./route/itemRoutes');
 
 connectDB();
 const app = express();
 
-app.get("/", (req, res) => res.send("Bravo my life!"));
+app.get('/', (req, res) => res.send('Bravo my life!'));
 
 app.use(express.json());
-app.use("/api/products", itemRoutes);
-const PORT = process.env.PORT || 5000;
+app.use('/api/products', itemRoutes);
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () =>
-	console.log(`Server is running on port ${PORT}`.yellow.bold)
+  console.log(`Server is running on port ${PORT}`.yellow.bold)
 );
