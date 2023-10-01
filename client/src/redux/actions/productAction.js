4ff6ed8bc5fc10d35bdeabc520fb3655af2c57e8
abiona01 <quadratwemimo@gmail.com>
2023-10-01 14:50:47 +0100

    env fixing

--- client/src/redux/actions/productAction.js
+++ client/src/redux/actions/productAction.js
@@ -1,7 +1,7 @@
 import * as actionTypes from '../types/productTypes';
 import axios from 'axios';
 
-export const BASE_ROUTE = process.env.API_URL || 'localhost:5000';
+export const BASE_ROUTE = process.env.REACT_APP_API_URL || 'localhost:5000';
 export const getProducts = () => async (dispatch) => {
   try {
     dispatch({
