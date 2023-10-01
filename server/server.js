060fe859bd41b2d477ed15454c56641c47dfd2db
abiona01 <quadratwemimo@gmail.com>
2023-10-01 14:17:38 +0100

    cors error fix

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const colors = require('colors');
+var cors = require('cors');
 const connectDB = require('./config/db');
 const itemRoutes = require('./route/itemRoutes');
 
 connectDB();
 const app = express();
-
+app.use(cors());
 app.get('/', (req, res) => res.send('Bravo my life!'));
 
 app.use(express.json());
 app.use('/api/products', itemRoutes);
 const PORT = process.env.PORT || 5000;
-console.log(PORT);
+
 app.listen(PORT, () =>
   console.log(`Server is running on port ${PORT}`.yellow.bold)
 );
