26c009e11fd56c1d29fb0397ea3f5eaa6a1f8c73
abiona01 <quadratwemimo@gmail.com>
2023-10-01 14:09:49 +0100

    log port

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -1,26 +1,27 @@
-import * as actionTypes from "../types/cartTypes";
-import axios from "axios";
+import * as actionTypes from '../types/cartTypes';
+import axios from 'axios';
+import { BASE_ROUTE } from './productAction';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-	const { data } = await axios.get(`/api/products/${id}`);
-	dispatch({
-		type: actionTypes.ADD_TO_CART,
-		payload: {
-			product: data._id,
-			name: data.name,
-			imageUrl: data.imageUrl,
-			price: data.price,
-			countInStock: data.countInStock,
-			qty,
-		},
-	});
-	localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
+  const { data } = await axios.get(`https://${BASE_ROUTE}/api/products/${id}`);
+  dispatch({
+    type: actionTypes.ADD_TO_CART,
+    payload: {
+      product: data._id,
+      name: data.name,
+      imageUrl: data.imageUrl,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+  localStorage.setItem('cart', JSON.stringify(getState().cart.cartItems));
 };
 
 export const removeFromCart = (id) => (dispatch, getState) => {
-	dispatch({
-		type: actionTypes.REMOVE_FROM_CART,
-		payload: id,
-	});
-	localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
+  dispatch({
+    type: actionTypes.REMOVE_FROM_CART,
+    payload: id,
+  });
+  localStorage.setItem('cart', JSON.stringify(getState().cart.cartItems));
 };
--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => res.send('Bravo my life!'));
 app.use(express.json());
 app.use('/api/products', itemRoutes);
 const PORT = process.env.PORT || 5000;
+console.log(PORT);
 app.listen(PORT, () =>
   console.log(`Server is running on port ${PORT}`.yellow.bold)
 );
