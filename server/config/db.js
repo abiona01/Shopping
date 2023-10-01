4508e1901da33fc410a7d4bf4cd2161fe280e420
abiona01 <quadratwemimo@gmail.com>
2023-10-01 13:49:01 +0100

    redeploy changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,18 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-const colors = require("colors");
+require('dotenv').config();
+const mongoose = require('mongoose');
+const colors = require('colors');
 
-const connectDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log("MongoDB connection SUCCESS".cyan.underline);
-	} catch (error) {
-		console.error("MongoDB connection FAIL");
-		process.exit(1);
-	}
+const connectDB = () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connection SUCCESS'.cyan.underline);
+  } catch (error) {
+    console.error('MongoDB connection FAIL');
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => res.send('Bravo my life!'));
 
 app.use(express.json());
 app.use('/api/products', itemRoutes);
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server is running on port ${PORT}`.yellow.bold)
 );
